f87c604f04cbd9e45e165e979c801b16877202fc
Loneth <odilio.ganesha@ti.ukdw.ac.id>
2026-05-25 02:11:00 +0700

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        // ini apa
         if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
